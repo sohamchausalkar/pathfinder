07dc09dd2fe0f706750786d99a3c2945fbfbbd23
Soham Chausalkar <138089333+sohamchausalkar@users.noreply.github.com>
2024-09-28 12:52:11 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,4 @@
-
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from data_preprocessing import load_data, preprocess_data, split_data
